ad81da8b646aae96049973b4c929959d7e3ae23a
Christian Pippin <ianpippin@gmail.com>
2020-03-05 10:31:21 -0600

    pushing to workout errors

--- bangazon-react-sprint1/src/components/products/ProductCard.js
+++ bangazon-react-sprint1/src/components/products/ProductCard.js
@@ -1,17 +1,17 @@
 import React, { Component } from "react"
 import { Button, Card } from 'react-bootstrap';
-import { Link } from 'react-router-dom'
 
 class Product extends Component {
 
   render() {
+    console.log("props", this.props)
     return (
       <>
         <section className="product">
           <Card style={{ width: '18rem' }}>
             <Card.Img variant="top" src={this.props.product.imagePath} />
             <Card.Body>
-              <Card.Title onClick={() => { this.props.history.push(`/products/${this.props.products.id}`) }}>{this.props.product.name} ${this.props.product.price}</Card.Title>
+              <Card.Title onClick={() => { this.props.history.push(`/products/${this.props.product.id}`) }}>{this.props.product.name} ${this.props.product.price}</Card.Title>
               <Card.Text>{this.props.product.description}</Card.Text>
               <Card.Text>{this.props.product.quantity} left to buy.</Card.Text>
               <Button variant="primary">Buy</Button>
